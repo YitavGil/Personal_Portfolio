39c29129de77c15c1cd3a417bd50c656ae26fc63
YitavGil <yitavga@yahoo.com>
2022-03-26 17:18:43 +0300

    completed about-skills-works

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BsFacebook, BsLinkedin } from 'react-icons/bs';
+
+const SocialMedia = () => {
+  return (
+    <div className='app__social'>
+        <div>
+            <BsFacebook />
+        </div>
+        <div>
+            <BsLinkedin />
+        </div>
+       
+
+    </div>
+  )
+}
+
+export default SocialMedia
